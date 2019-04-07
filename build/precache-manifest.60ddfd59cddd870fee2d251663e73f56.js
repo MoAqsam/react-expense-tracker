88c79723eb09d4dcda96a08547d5a4f7a49257c5
MoAqsam <MoChainz@localhost.com>
2019-04-06 21:17:58 -0400

    new build

---
+++ build/precache-manifest.60ddfd59cddd870fee2d251663e73f56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f1231dbe6b237bd306a",
+    "url": "/react-expense-tracker/static/js/runtime~main.a1159160.js"
+  },
+  {
+    "revision": "b7ffde5f0ef92403e793",
+    "url": "/react-expense-tracker/static/js/main.b93e02f7.chunk.js"
+  },
+  {
+    "revision": "da20293e4a870c61dfac",
+    "url": "/react-expense-tracker/static/js/2.1d59d780.chunk.js"
+  },
+  {
+    "revision": "b7ffde5f0ef92403e793",
+    "url": "/react-expense-tracker/static/css/main.19d09dbb.chunk.css"
+  },
+  {
+    "revision": "5f4ecce8bcef066ac675f95444f66114",
+    "url": "/react-expense-tracker/index.html"
+  }
+];
